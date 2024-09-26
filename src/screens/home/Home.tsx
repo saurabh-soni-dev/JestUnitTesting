a8a530e0d432246967480d3511b9e131dd5db4f7
saurabh-soni-dev <saurabh.soni6408@gmail.com>
2024-09-26 22:12:29 +0530

    First Commit

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import { View, StatusBar } from 'react-native';
 import React from 'react';
+import { styles } from './home.style';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
     </View>
   );
 };
